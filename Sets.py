eda434365f16c8c957adb5fac3edff80b05d084e
Felix Ibitoroko Felix <Ibitoroko.Felix@nnpcgroup.com>
2025-08-09 13:58:35 +0100

    Learnt some basic set operations in python

---
+++ Sets.py
@@ -0,0 +1,11 @@
+setStudID = {'csi001', 'csi002', 'csi003', 'csi004', 'csi005'} #creates a set of student IDs
+print(setStudID)  #outputs the set of student IDs
+
+setStaffID = {'felb.b0001', 'felb.b0002', 'felb.b0003', 'felb.b0004', 'felb.b0005'} #creates a set of staff IDs
+print(setStaffID)  #outputs the set of staff IDs
+
+mixedSet = {'bread', 484, 'milk', 3.14, 'eggs'}  #creates a mixed set with different data types
+print(mixedSet)  #outputs the mixed set
+
+emptySet = set{}  #creates an empty set
+print(emptySet)  #outputs the empty set
